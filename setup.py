f5b11d70f95f6d2061c5e6329c1efee15591fb55
강태욱(KANG TAEWOOK)/플랫폼_Tech1그룹/SK <wecanfly@sk.com>
2020-10-20 14:12:35 +0900

    1.0.5

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_namespace_packages
 setup(
     name="autoinsight_helpers",
-    version="1.0.4",
+    version="1.0.5",
     packages=find_namespace_packages(),
     include_package_data=True,
 )
