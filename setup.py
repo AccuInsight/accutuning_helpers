ac820c2adc525c203610936fd46c6a55f15a7ccd
happytk <rainyblue@gmail.com>
2020-11-24 10:38:18 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_namespace_packages
 setup(
     name="autoinsight_helpers",
-    version="1.0.8",
+    version="1.0.9",
     packages=find_namespace_packages(),
     include_package_data=True,
 )
