2ddf63b7c73f211203a47de9ea6066ba8aabbfae
Taewook Kang <rainyblue@gmail.com>
2020-12-09 10:54:20 +0900

    1.0.11

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_namespace_packages
 setup(
     name="autoinsight_helpers",
-    version="1.0.10",
+    version="1.0.11",
     packages=find_namespace_packages(),
     include_package_data=True,
 )
