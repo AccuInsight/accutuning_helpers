95cd608b20b95581dfd5106b352f3f95dad2cd7d
Taewook Kang <rainyblue@gmail.com>
2020-12-10 19:20:57 +0900

    add vectorizer

--- autoinsight_helpers/feature_engineering/nlp.py
+++ autoinsight_helpers/feature_engineering/nlp.py
@@ -1,5 +1,4 @@
 from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.preprocessing import OneHotEncoder
 from sentence_transformers import (
     models,
     # losses,
@@ -11,14 +10,9 @@ import pathlib
 import pickle
 
 
-class AutoinsightLabeler(BaseEstimator, TransformerMixin):
-    def __init__(self, feature_name, classifier_fp, classifier_label_fp):
+class AutoinsightVectorizer(BaseEstimator, TransformerMixin):
+    def __init__(self, feature_name):
         self.feature_name = feature_name
-        self.ohe = OneHotEncoder(sparse=False)
-        self.classifier_fp = pathlib.Path(classifier_fp)
-        self.classifier = pickle.loads(self.classifier_fp.read_bytes())
-        self.classifier_label_fp = pathlib.Path(classifier_label_fp)
-        self.labels = pickle.loads(self.classifier_label_fp.read_bytes())
 
         word_embedding_model = models.BERT('bert-base-multilingual-cased')
         pooling_model = models.Pooling(
@@ -28,16 +22,8 @@ class AutoinsightLabeler(BaseEstimator, TransformerMixin):
             pooling_mode_max_tokens=False)
         self.model = SentenceTransformer(modules=[word_embedding_model, pooling_model])
 
-    def fit(self, X, y=0, **fit_params):
-        return self
-
-    def transform(self, X, y=0):
+    def _vectorize(self, X):
         np.set_printoptions(threshold=100)
-
-        # logging.basicConfig(format='%(asctime)s - %(message)s',
-        #                     datefmt='%Y-%m-%d %H:%M:%S',
-        #                     level=logging.INFO,
-        #                     handlers=[LoggingHandler()])
         corpus_embeddings = self.model.encode(
             [
                 str(stc)
@@ -48,10 +34,34 @@ class AutoinsightLabeler(BaseEstimator, TransformerMixin):
             f'{self.feature_name}_{i}'
             for i in range(corpus_embeddings.shape[1])
         ]
-        vector_df = pd.DataFrame(
+        return pd.DataFrame(
             corpus_embeddings,
             columns=columns
         )
+
+    def fit(self, X, y=0, **fit_params):
+        return self
+
+    def transform(self, X, y=0):
+        vector_df = self._vectorize(X)
+        X = X.drop(self.feature_name, axis=1)
+        return pd.concat(
+            [X, vector_df],
+            axis=1
+        )
+
+
+class AutoinsightLabeler(AutoinsightVectorizer):
+    def __init__(self, feature_name, classifier_fp, classifier_label_fp):
+        super().__init__(feature_name)
+
+        self.classifier_fp = pathlib.Path(classifier_fp)
+        self.classifier = pickle.loads(self.classifier_fp.read_bytes())
+        self.classifier_label_fp = pathlib.Path(classifier_label_fp)
+        self.labels = pickle.loads(self.classifier_label_fp.read_bytes())
+
+    def transform(self, X, y=0):
+        vector_df = self._vectorize(X)
         tags = self.classifier.predict(vector_df)
         tags = [
             self.labels[int(tag)]
