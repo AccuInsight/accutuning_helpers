2ccf8b2dcab8ab6bc1ec5ac99615a47ccd0b5f3c
Sehyeong Kim <kseh92@gmail.com>
2021-04-14 13:48:06 +0900

    v1.0.21 setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_namespace_packages
 setup(
     name="accutuning_helpers",
-    version="1.0.20",
+    version="1.0.21",
     packages=find_namespace_packages(),
     include_package_data=True,
 )
