500eb3bbe07378eede6740260a1114a5c08e7099
ahaljh <ahaljh@gmail.com>
2022-01-21 00:16:28 +0900

    package version update

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_namespace_packages
 setup(
     name="accutuning_helpers",
-    version="1.0.28",
+    version="1.0.29",
     packages=find_namespace_packages(),
     include_package_data=True,
     install_requires=[
-        "numpy==1.18.1",
-        "pandas==1.1.4",
-        "scikit-learn==0.23.2",
-        "scipy==1.4.1",
-        "joblib==0.16.0",
-        "python-dateutil==2.8.1",
+        "numpy==1.21.4",
+        "pandas==1.3.4",
+        "scikit-learn==1.0.1",
+        "scipy==1.7.2",
+        "joblib==1.1.0",
+        "python-dateutil==2.8.2",
         "ctgan==0.3.1",
         "sentence-transformers==0.3.2",
     ]
