50783c275ff8761a28b2a91db3321d14a16fc57d
Hyeonho Lee <40379485+hyeonho1028@users.noreply.github.com>
2022-06-17 17:42:09 +0900

    rollback
    변경 후에도 같은 현상이 나타나 원래 코드로 rollback합니다.

--- accutuning_helpers/preprocessing/dtypeconvert.py
+++ accutuning_helpers/preprocessing/dtypeconvert.py
@@ -1,35 +1,26 @@
 import logging
 import math
-from typing import Tuple, Literal, Dict, List, Union, Iterable
-
-import numpy as np
+from typing import Tuple, Literal, Dict, List
 import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
 
 logger = logging.getLogger(__name__)
 
 TEXT_CONVERTER = Literal['tfidf', 'BERT', 'zero_shot', 'few_shot']
-
-
 ##########################################################################
 # preprocessor 화면에서 받은 datatype_pair_match에 따라서 각 column들의 datatype을 변경합니다.
 ##########################################################################
 class AccutuningDtypeConvert(BaseEstimator, TransformerMixin):
-
 	def __init__(
 			self,
 			datatype_pair_match: List[Tuple[str, str]],
-			text_converter: TEXT_CONVERTER = 'tfidf',
-			text_vectorizer=None
+			text_converter: TEXT_CONVERTER = 'tfidf'
 	):
 		self.datatype_pair_match: Tuple[str, str] = datatype_pair_match
 		self._converter = text_converter
-		self.text_vectorizer = text_vectorizer
 		self._vector_dict: Dict[str, "TokenEmbedderBase"] = dict()
-
 	def fit(self, X: pd.DataFrame, y=0, **fit_params) -> "AccutuningDtypeConvert":
 		return self
-
 	def transform(self, X: pd.DataFrame, y=0) -> pd.DataFrame:
 		X_tr = X.copy()
 		for (col, typ) in self.datatype_pair_match:
@@ -44,12 +35,10 @@ class AccutuningDtypeConvert(BaseEstimator, TransformerMixin):
 					ratio = 0.001
 					min_df = min(math.floor(len(converting_col) * ratio), 10)
 					vec = self._vectorizer_factory(col, min_df=min_df)
-
 					if vec.embedding_length == 0:  # not fitted
 						X_tr = vec.fit_transform(X_tr, y=y)
 					else:
 						X_tr = vec.transform(X_tr)
-
 				elif str(converting_col.dtype) != typ:
 					if typ == 'datetime64':
 						pass
@@ -64,21 +53,17 @@ class AccutuningDtypeConvert(BaseEstimator, TransformerMixin):
 								f'Failed to convert the datatype of column {col} to {str(typ)}. Set to default.'
 							)
 		return X_tr
-
 	def _vectorizer_factory(self, col: str, **params) -> "TokenEmbedderBase":
 		if col in self._vector_dict:
 			return self._vector_dict[col]
 		else:  # register vectorizer
 			if self._converter == 'tfidf':
-# 				from accutuning_helpers.text.embedder_tfidf import TfIdfTokenVectorizer
-# 				vec = TfIdfTokenVectorizer(feature_name=col, **params)
-				vec = self.text_vectorizer(feature_name=col, **params)
+				from accutuning_helpers.text.embedder_tfidf import TfIdfTokenVectorizer
+				vec = TfIdfTokenVectorizer(feature_name=col, **params)
 			elif self._converter == 'BERT':
 				## FIXME - flair version 에 맞게 huggingface version 4로 version up
 				## FIXME - sentence transformer 기반 BERT vectorizer는 deprecated 될 예정
-# 				from accutuning_helpers.text.embedder_bert import BERTVectorizer
-# 				vec = BERTVectorizer(feature_name=col)
-				vec = self.text_vectorizer(feature_name=col)
+				from accutuning_helpers.text.embedder_bert import BERTVectorizer
+				vec = BERTVectorizer(feature_name=col)
 			self._vector_dict[col] = vec
 			return vec
-
