a72bba2904ce7a44936ca668047b00a8a688e227
Hyeonho Lee <40379485+hyeonho1028@users.noreply.github.com>
2022-06-17 17:01:04 +0900

    Update dtypeconvert.py
    recognized local variable to accutuning

--- accutuning_helpers/preprocessing/dtypeconvert.py
+++ accutuning_helpers/preprocessing/dtypeconvert.py
@@ -1,11 +1,19 @@
 import logging
 import math
-from typing import Tuple, Literal, Dict, List
+from typing import Tuple, Literal, Dict, List, Union, Iterable
 
+import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
 
-from accutuning_helpers.text.embedder_tfidf import TfIdfTokenVectorizer
+from flair.data import Sentence, Tokenizer
+from flair.embeddings.document import DocumentEmbeddings
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+from accutuning_helpers.text.embedder import TokenEmbedderBase
+from accutuning_helpers.text.tokenizer import KonlpyTokenizer
+
+# from accutuning_helpers.text.embedder_tfidf import TfIdfTokenVectorizer
 from accutuning_helpers.text.embedder_bert import BERTVectorizer
 
 logger = logging.getLogger(__name__)
@@ -79,3 +87,71 @@ class AccutuningDtypeConvert(BaseEstimator, TransformerMixin):
 				vec = BERTVectorizer(feature_name=col)
 			self._vector_dict[col] = vec
 			return vec
+
+		
+
+class TfIdfTokenVectorizer(TokenEmbedderBase, DocumentEmbeddings):
+
+	def __init__(
+			self,
+			feature_name: str,
+			tokenizer: Tokenizer = KonlpyTokenizer('MeCab'),
+			decode_error="replace",
+			lowercase=False,
+			min_df=10,  # configurable
+			**vectorizer_params,
+	):
+		super(TfIdfTokenVectorizer, self).__init__(feature_name)
+		vec = TfidfVectorizer(
+			decode_error=decode_error,
+			min_df=min_df,
+			tokenizer=lambda x: tokenizer.tokenize(x),
+			**vectorizer_params,
+		)
+		self._tokenizer = tokenizer
+		self._vectorizer = vec
+		self.name: str = "accutuning_text_tfidf"
+
+	def fit(self, df: pd.DataFrame, y=None) -> "TfIdfTokenVectorizer":
+		texts = df[self.feature_name].tolist()
+		self._vectorizer.fit(texts, y=y)
+		return self
+
+	def vectorize(self, df: pd.DataFrame) -> pd.DataFrame:
+		# np.set_printoptions(threshold=100)
+		corpus_embeddings = self.encode(df[self.feature_name].tolist())
+		# columns = [
+		# 	f'{self.feature_name}_{i}'
+		# 	for i in range(corpus_embeddings.shape[1])
+		# ]
+		columns = self._vectorizer.get_feature_names()
+		new_df = pd.DataFrame.sparse.from_spmatrix(corpus_embeddings, columns=columns)
+		return new_df
+
+	def encode(self, sentences: Iterable[str]) -> np.ndarray:
+		# documents = [self._tokenizer.tokenize(text) for text in X]
+		# return self._vectorizer.transform(documents)
+		return self._vectorizer.transform(sentences)
+
+	@property
+	def tokenizer(self) -> Tokenizer:
+		return self._tokenizer
+
+	@property
+	def embedding_length(self) -> int:
+		vec = self._vectorizer
+		return 0 if not hasattr(vec, 'vocabulary_') else len(vec.vocabulary_)
+
+	def embed(self, sentences: Union[List[Sentence], Sentence]):
+		"""Add embeddings to every sentence in the given list of sentences."""
+
+		# if only one sentence is passed, convert to list of sentence
+		if isinstance(sentences, Sentence):
+			sentences = [sentences]
+
+		import torch
+		raw_sentences = [s.to_original_text() for s in sentences]
+		tfidf_vectors = torch.from_numpy(self._vectorizer.transform(raw_sentences).A)
+
+		for sentence_id, sentence in enumerate(sentences):
+			sentence.set_embedding(self.name, tfidf_vectors[sentence_id])
