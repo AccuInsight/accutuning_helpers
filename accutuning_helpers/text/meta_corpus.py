731d9cbf5b11d7d4d9f59e6ba86129f27f7cc1e6
keanu-paik <yspaik@gmail.com>
2022-06-27 06:07:28 +0900

    change params back to electra

--- accutuning_helpers/text/meta_corpus.py
+++ accutuning_helpers/text/meta_corpus.py
@@ -305,13 +305,14 @@ class BaseMetaLearner(MetaLearner):
 if __name__ == "__main__":
 	meta = BaseMetaLearner(
 		model_path=None,  # base learning
-		max_epochs=30,
+		max_epochs=10,
 		mini_batch_size=16,
 		mini_batch_chunk_size=8,
 		learning_rate=0.02,
 		train_with_dev=False,
 	)
-	# result = meta.base_learning(down_sample=0.1, embedding="kykim/bert-kor-base")
-	result = meta.base_learning(down_sample=0.1, embedding="klue/bert-base")
+	# result = meta.base_learning(down_sample=1.0, embedding="kykim/bert-kor-base")
+	result = meta.base_learning(down_sample=1.0, embedding="kykim/electra-kor-base")
+	# result = meta.base_learning(down_sample=1.0, embedding="klue/bert-base")
 	path = meta.save_model()
 	print(path)
