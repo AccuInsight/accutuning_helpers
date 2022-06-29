7892d4c0618438bccf8971dc2774495f0334d2a1
keanu-paik <yspaik@gmail.com>
2022-06-29 23:54:21 +0900

    update scheduler

--- accutuning_helpers/text/meta_corpus.py
+++ accutuning_helpers/text/meta_corpus.py
@@ -1,12 +1,13 @@
 import logging
+import math
 from copy import copy
 from typing import Dict, List
 
 import datasets
-import transformers
 from flair.data import Corpus, Sentence, Tokenizer
 from flair.datasets.document_classification import FlairDataset
 from flair.models import TARSClassifier
+from flair.optim import LinearSchedulerWithWarmup
 from flair.tokenization import SegtokTokenizer
 from flair.trainers import ModelTrainer
 from torch.optim import AdamW
@@ -267,8 +268,10 @@ class BaseMetaLearner(MetaLearner):
 		no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
 		decay = 0.01
 		params = [
-			{'params': [p for n, p in _params if not any(nd in n for nd in no_decay)], 'weight_decay': decay},
-			{'params': [p for n, p in _params if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
+			{'params': [p for n, p in _params if not any(nd in n for nd in no_decay)], 'lr': 0.01,
+			 'weight_decay': decay},
+			{'params': [p for n, p in _params if any(nd in n for nd in no_decay)], 'lr': self._learning_rate,
+			 'weight_decay': 0.0}
 		]
 		optimizer = AdamW(params, lr=self._learning_rate, weight_decay=decay)
 
@@ -295,20 +298,20 @@ class BaseMetaLearner(MetaLearner):
 					)
 
 				# initialize the text classifier trainer with corpus
-				# total_steps = len(c.train) * self._max_epochs
-				# scheduler = transformers.get_linear_schedule_with_warmup(
-				# 	optimizer,
-				# 	num_warmup_steps=self._warmup_fraction * total_steps,
-				# 	num_training_steps=total_steps,
-				# )
+				total_steps = math.ceil(len(c.train) / self._mini_batch_size) * self._max_epochs
+				scheduler = LinearSchedulerWithWarmup(
+					optimizer=optimizer,
+					num_train_steps=total_steps,
+					num_warmup_steps=self._warmup_fraction * total_steps,
+				)
 
 				trainer = ModelTrainer(tars, c)
 				result = trainer.train(
 					base_path=self._output_path / c.name,  # path to store the model artifacts
 					learning_rate=self._learning_rate,  # use very small learning rate
-					optimizer=optimizer,
-					# scheduler=scheduler,
 					# optimizer=Adam, # default SGD
+					optimizer=optimizer,
+					scheduler=scheduler,
 					mini_batch_size=self._mini_batch_size,  # small mini-batch size since corpus is tiny
 					patience=self._patience,
 					warmup_fraction=self._warmup_fraction,
