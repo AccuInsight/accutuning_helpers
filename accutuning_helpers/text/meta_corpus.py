edfbadde401aabf358ed8583fdf1cafaac5d6de5
keanu-paik <yspaik@gmail.com>
2022-07-01 08:49:42 +0900

    lr 1e-4

--- accutuning_helpers/text/meta_corpus.py
+++ accutuning_helpers/text/meta_corpus.py
@@ -334,7 +334,8 @@ if __name__ == "__main__":
 		mini_batch_size=16,
 		mini_batch_chunk_size=4,
 		# learning_rate=1e-4,
-		learning_rate=7e-5,
+		# learning_rate=7e-5,
+		learning_rate=1e-4,
 		# learning_rate=5e-5,  # learning rate
 		# learning_rate=5e-3,
 		# learning_rate=0.02,
