44237c2e8cb67cb51194038f0c9b7e0105c69fb4
keanu-paik <yspaik@gmail.com>
2022-07-05 17:46:53 +0900

    output.json에 output_fp, execution_time 추가 관련사항 단위테스트 추가

--- tests/text/test_meta_learning.py
+++ tests/text/test_meta_learning.py
@@ -1,16 +1,16 @@
-import os
 import json
+import os
 from unittest import TestCase
 
 from accutuning_helpers.text import utils as labeler_utils
-from accutuning_helpers.text.meta_learning import MetaLearner, WORKPLACE_PATH
+from accutuning_helpers.text.meta_learning import MetaLearner, WORKPLACE_PATH, WORKPLACE_HOME
 
 
 class TestMetaLearner(TestCase):
 
 	def setUp(self) -> None:
 		self.meta = MetaLearner(
-			model_path=os.path.join(WORKPLACE_PATH, 'output/fine_tuned_ko.pt'),
+			model_path=os.path.join(WORKPLACE_PATH, 'output/tars-electra-ko-v1.pt'),
 			max_epochs=10,
 			train_with_dev=True,
 		)
@@ -18,7 +18,7 @@ class TestMetaLearner(TestCase):
 	# def test_fine_tuning(self):
 	# 	assert False
 	#
-	def test_zero_shot_learning(self):
+	def test_label_predict(self):
 		conf = {
 			"random_seed": 42,
 			"source_data_fp": "data/nnst_lt_10.csv",
@@ -33,16 +33,40 @@ class TestMetaLearner(TestCase):
 			"labeler_worker_type": "zsl"
 		}
 		meta = self.meta
-		result = meta.zero_shot_learning(**conf)
-		result.pop('texts') # too long to print
+		result = meta.label_predict(**conf)
+		result.pop('texts')  # too long to print
 		result['predictions'] = [p.value for p in result['predictions']]
-		print(json.dumps(result, indent=2, ensure_ascii=False),)
+		print(json.dumps(result, indent=2, ensure_ascii=False), )
 		# assert all(map(lambda x: x in conf['class_nm_list'], result['predictions']))
 
 		input_path = os.path.join(WORKPLACE_PATH, conf['source_data_fp'])
 		target_df = labeler_utils.load(input_path)
 		gold, pred = target_df['tags'], result['predictions']
 		labeler_utils.evaluate(gold, pred)
+
+	def test_save_results(self):
+		conf = {
+			"random_seed": 42,
+			"source_data_fp": "data/nnst_lt_10.csv",
+			# "source_data_fp": "data/nnst_lt_1990.csv",
+			"samples_fp": "",
+			"related_stcs": "",
+			"correct": True,
+			"class_nm_list": ["생활", "기술"],
+			"target_column_nm": "stcs",
+			"samples_target_column_nm": "내용",
+			"samples_tag_column_nm": "라벨",
+			"labeler_worker_type": "zsl"
+		}
+		meta = self.meta
+		result = meta.label_predict(**conf)
+		output_path_info = meta.save_result(
+			result_csv_filename='test.csv',
+			**result
+		)
+		assert 'execution_time' in output_path_info
+		assert os.path.exists(os.path.join(WORKPLACE_HOME, output_path_info['output_fp']))
+		print(json.dumps(output_path_info, indent=2, ensure_ascii=False), )
 
 	def test_few_shot_learning(self):
 		conf = {
@@ -60,7 +84,7 @@ class TestMetaLearner(TestCase):
 		}
 		meta = self.meta
 		result = meta.few_shot_learning(**conf)
-		result.pop('texts') # too long to print
+		result.pop('texts')  # too long to print
 		result['predictions'] = [p.value for p in result['predictions']]
 		# print(json.dumps(result, indent=2, ensure_ascii=False),)
 
@@ -77,8 +101,31 @@ class TestMetaLearner(TestCase):
 		# labeler_utils.evaluate(gold, pred)
 		assert result
 
-	# def test_label_predict(self):
-	# 	assert False
+	def test_zero_shot_learning(self):
+		conf = {
+			"random_seed": 42,
+			"source_data_fp": "data/nnst_lt_10.csv",
+			# "source_data_fp": "data/nnst_lt_1990.csv",
+			"samples_fp": "",
+			"related_stcs": "",
+			"correct": True,
+			"class_nm_list": ["생활", "기술"],
+			"target_column_nm": "stcs",
+			"samples_target_column_nm": "내용",
+			"samples_tag_column_nm": "라벨",
+			"labeler_worker_type": "zsl"
+		}
+		meta = self.meta
+		result = meta.zero_shot_learning(**conf)
+		result.pop('texts')  # too long to print
+		result['predictions'] = [p.value for p in result['predictions']]
+		print(json.dumps(result, indent=2, ensure_ascii=False), )
+		# assert all(map(lambda x: x in conf['class_nm_list'], result['predictions']))
+
+		input_path = os.path.join(WORKPLACE_PATH, conf['source_data_fp'])
+		target_df = labeler_utils.load(input_path)
+		gold, pred = target_df['tags'], result['predictions']
+		labeler_utils.evaluate(gold, pred)
 
 
 class TestMetaBaseLearner(TestCase):
