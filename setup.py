289fbd8ec59f4d0f502f6c4572529c07c35524db
sejin Oh <sjin4you@sk.com>
2022-08-03 15:45:36 +0900

    1.0.32 version update

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from setuptools import setup, find_namespace_packages
 setup(
     name="accutuning_helpers",
-    version="1.0.31",
+    version="1.0.32",
     packages=find_namespace_packages(),
     include_package_data=True,
     install_requires=[
         "numpy==1.19.5",
         "pandas==1.1.4",
-        "scikit-learn==0.23.2",
         "scipy==1.5.2",
         "joblib==1.1.0",
-        "python-dateutil==2.8.2",
+        "scikit-learn==0.23.2",
+        "xgboost==1.6.1",
+        "lightgbm==3.3.2"
         "ctgan==0.4.3",
-        "xgboost==1.5.0",
-        "lightgbm>=2.3.1"
+        "python-dateutil==2.8.2",
         # text added
         "konlpy==0.6",
         "customized_konlpy",
