c185a56c9c6695aea9e12a77a7d5f0430f63d971
Hyanggon <144635540+Hyanggon@users.noreply.github.com>
2024-01-11 10:41:31 +0900

    Labeler 관련 제거

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_namespace_packages
 setup(
     name="accutuning_helpers",
-    version="1.0.34",
+    version="1.0.33",
     packages=find_namespace_packages(),
     include_package_data=True,
     install_requires=[
