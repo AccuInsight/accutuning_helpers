2be235b37e726267ba5a01a89fce388bf9fb21b2
hyanggonjin <hygn3134@sk.com>
2024-02-22 09:45:09 +0900

    Labeler 제거

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_namespace_packages, setup
 
 setup(
     name="accutuning_helpers",
-    version="1.0.33",
+    version="1.1.0",
     packages=find_namespace_packages(),
     include_package_data=True,
     install_requires=[
